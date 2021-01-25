665ed6984f28fd8cf3a58863d95352b918fa00a8
anhnguyen21 <anhanh5811@gmail.com>
2021-01-25 11:21:20 +0700

    add file colors

---
+++ src/themes/Colors.js
@@ -0,0 +1,11 @@
+const colors={
+    primary: '#fc9619',
+    secondary: '#39b8c2',
+    btnLevel1: '##5f5f5f',
+    greyishBrown:'#4a4a4a',
+    waterBlueTwo: '#1d9dd8',
+    txtLevel2: '#ababab',
+    txtLevel3:'#7f7f7f',
+}
+
+export default colors;
