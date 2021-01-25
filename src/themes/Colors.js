032fd2b52297a1c34d52a60aab93f0d8aff6b165
anhnguyen21 <anhanh5811@gmail.com>
2021-01-25 11:14:14 +0700

    add color

--- src/themes/Colors.js
+++ src/themes/Colors.js
@@ -1,8 +1,11 @@
 const colors={
     primary: '#fc9619',
     secondary: '#39b8c2',
-    warmGrey:'#7f7f7f',
+    btnLevel1: '##5f5f5f',
     greyishBrown:'#4a4a4a',
+    waterBlueTwo: '#1d9dd8',
+    txtLevel2: '#ababab',
+    txtLevel3:'#7f7f7f',
 }
 
 export default colors;
