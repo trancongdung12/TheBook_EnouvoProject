be34a40e06a2eb74c98bbd95ea4c13f369234ab6
anhnguyen21 <anhanh5811@gmail.com>
2021-01-25 16:38:16 +0700

    add intro

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/screens/Home';
+import App from './src/screens/Intro/index';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
